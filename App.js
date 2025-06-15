214680072dad2dd2d9027c045d72fc5cd21e91a2
Mukund Kumar Verma <21mukund.29@gmail.com>
2025-06-15 23:58:21 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [total, setTotal] = useState(0);
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
