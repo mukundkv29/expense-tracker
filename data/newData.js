e5f5e530020222cbad9509ee36b2079cada3ee64
Mukund Kumar Verma <21mukund.29@gmail.com>
2025-07-21 22:00:39 +0530

    fix: added more items in newData.js

--- data/newData.js
+++ data/newData.js
@@ -9,4 +9,34 @@ export const initialItems = [
     value: "",
     expenses: [],
   },
+  {
+    name: "Groceries",
+    value: "",
+    expenses: [],
+  },
+  {
+    name: "Electronics",
+    value: "",
+    expenses: [],
+  },
+  {
+    name: "Sports",
+    value: "",
+    expenses: [],
+  },
+  {
+    name: "Entertainment",
+    value: "",
+    expenses: [],
+  },
+  {
+    name: "Rent",
+    value: "",
+    expenses: [],
+  },
+  {
+    name: "Transport",
+    value: "",
+    expenses: [],
+  }
 ];
