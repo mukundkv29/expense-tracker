33fca74ada617eaeebd33f4109721cfa60113eee
Mukund Kumar Verma <21mukund.29@gmail.com>
2025-08-05 13:19:49 +0530

    fix: removed unused expenses and total properties of expense-items

--- data/newData.js
+++ data/newData.js
@@ -2,49 +2,33 @@ export const initialItems = [
   {
     name: "Shopping",
     value: "",
-    expenses: [],
-    total: 0,
   },
   {
     name: "Trips",
     value: "",
-    expenses: [],
-    total: 0,
   },
   {
     name: "Groceries",
     value: "",
-    expenses: [],
-    total: 0,
   },
   {
     name: "Electronics",
     value: "",
-    expenses: [],
-    total: 0,
   },
   {
     name: "Sports",
     value: "",
-    expenses: [],
-    total: 0,
   },
   {
     name: "Entertainment",
     value: "",
-    expenses: [],
-    total: 0,
   },
   {
     name: "Rent",
     value: "",
-    expenses: [],
-    total: 0,
   },
   {
     name: "Transport",
     value: "",
-    expenses: [],
-    total: 0,
   }
 ];
